e8006176a5d84c879ff97fb19ffe74e6928c7c5c
Christian Dodart <dodartchristian@gmail.com>
2021-04-05 14:31:35 -0600

    gameState fix

--- script.js
+++ script.js
@@ -140,6 +140,7 @@ const gameOver = function () {
     document.getElementById("btnHold").textContent = playerName2 + " Loses!";
     document.getElementById("btnRoll").classList.add("btnText");
     document.getElementById("btnHold").classList.add("btnText");
+    gameState = false;
     currentScore1.textContent = 0;
     currentScore2.textContent = 0;
 
